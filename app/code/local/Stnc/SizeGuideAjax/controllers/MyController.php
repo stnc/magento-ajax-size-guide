91bcbf97032672007153ef4f1c4c9ef9174044f3
local <selmantunc@yandex.com>
2017-09-19 12:00:13 +0300

    v1.0 addd

---
+++ app/code/local/Stnc/SizeGuideAjax/controllers/MyController.php
@@ -0,0 +1,16 @@
+﻿<?php
+class Stnc_SizeGuideAjax_MyController extends Mage_Core_Controller_Front_Action
+{
+    public function sizeAction()
+    {
+/*
+        echo Mage::getStoreConfig('stnc/stnc_group/stnc_select',Mage::app()->getStore());;
+        echo
+        echo Mage::getStoreConfig('stnc/stnc_group/stnc_input_BlockName',Mage::app()->getStore());;
+*/
+       $blockID= Mage::getStoreConfig('stnc/stnc_group/stnc_inputBlockid',Mage::app()->getStore());;
+        echo $this->getLayout()->createBlock('cms/block')->setBlockId($blockID)->toHtml();
+
+
+    }
+}
